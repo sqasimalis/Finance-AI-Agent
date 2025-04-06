31f6d3146f9eb5fa5ad8f1aba014112d7f0be80c
Syed Qasim Ali Shah <syedqasimali311@gmail.com>
2025-04-06 17:05:42 +0200

    AI Agents

---
+++ 1_simple_groq_agent.py
@@ -0,0 +1,11 @@
+from phi.agent import Agent
+from phi.model.groq import Groq
+from dotenv import load_dotenv
+
+load_dotenv()
+
+agent = Agent(
+    model=Groq(id="llama-3.3-70b-versatile")
+)
+
+agent.print_response("Write 2 sentence poem for the love between chocolate and ice cream")
